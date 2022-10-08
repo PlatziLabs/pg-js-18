ed3747ea13eabfe32f95084ead790aeeb68790f3
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-08 15:45:25 -0500

    save

--- src/exercise.js
+++ src/exercise.js
@@ -1,3 +1,40 @@
-export function combinationSum(array, target) {
-  // Tu código aquí 👈
+export function combinationSum(candidates, target) {
+  let remainingSum = target;
+  let finalCombinations = [];
+  let currentCombination = [];
+  let startFrom = 0;
+
+  for (let i = 0; i < candidates.length; i++) {
+    remainingSum = remainingSum - candidates[i];
+    currentCombination.push(candidates[i]);
+    while (startFrom < candidates.length) {
+      remainingSum = remainingSum - candidates[startFrom];
+      if (remainingSum === 0) {
+        currentCombination.push(candidates[startFrom]);
+        finalCombinations.push(currentCombination.sort());
+        currentCombination = [];
+        startFrom++;
+        remainingSum = target;
+      } else if (remainingSum > 0 && remainingSum >= candidates[startFrom]) {
+        currentCombination.push(candidates[startFrom]);
+      } else {
+        remainingSum = remainingSum + candidates[startFrom];
+        startFrom++;
+      };
+    };
+    currentCombination = [];
+    remainingSum = target;
+    startFrom = 0;
+  };
+
+  currentCombination = finalCombinations.sort();
+  finalCombinations = [];
+
+  for (let i = 0; i < currentCombination.length; i++) {
+    if (JSON.stringify(currentCombination[i]) !== JSON.stringify(currentCombination[i - 1])) {
+      finalCombinations.push(currentCombination[i]);
+    };
+  };
+
+  return finalCombinations;
 }
